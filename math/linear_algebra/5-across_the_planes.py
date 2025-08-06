1ed8db1570804f51d1957018ab64e1a61a2e86da
Kazim <kazimammadli@gmail.com>
2025-08-06 12:53:05 +0400

    correction of indentation

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,7 +6,7 @@ to add two 2D matrices of ints/floats."""
 def add_matrices2D(mat1, mat2):
     """Return the sum of two matrices."""
     if (len(mat1) != len(mat2) or
-        len(mat1[0]) != len(mat2[0])):
+            len(mat1[0]) != len(mat2[0])):
         return None
     return [[mat1[i][j] + mat2[i][j]
             for j in range(len(mat1[0]))]
