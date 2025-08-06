ade937862906776cceb50c910522edc23bb5ebe8
Kazim <kazimammadli@gmail.com>
2025-08-06 20:03:57 +0400

    fixing size problem

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,9 +4,8 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """Return concatened matrix"""
-    if axis not in (0, 1):
-        return None
-    
-    return ([row[:] for row in mat1] + [row[:] for row in mat2]
-            if axis == 0
-            else ([mat1[i][:]+mat2[i][:] for i in range(len(mat1))]))
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        return [row[:] for row in mat1] + [row[:] for row in mat2]
+    if axis == 1 and len(mat1) == len(mat2):
+        return [mat1[i][:]+mat2[i][:] for i in range(len(mat1))]
+    return None
