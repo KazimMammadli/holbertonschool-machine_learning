080809f9a648ee1947682c526b02ce284af36dc9
Kazim <kazimammadli@gmail.com>
2025-08-06 21:06:57 +0400

    11.task

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3
+"""
+This module defines a function
+to transpose a matrix.
+"""
+
+
+def np_transpose(matrix):
+    """Return transpose of a matrix."""
+    return matrix.T
