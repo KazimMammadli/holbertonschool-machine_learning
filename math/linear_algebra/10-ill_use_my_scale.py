4aefc4728af1fd47fe6af44e756caea0e418f59a
Kazim <kazimammadli@gmail.com>
2025-08-06 21:03:13 +0400

    10.task

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module defines a function to determine shape of the matrix."""
+
+
+def np_shape(matrix):
+    """Returns the shape of the matrix as a tuple."""
+    return matrix.shape
