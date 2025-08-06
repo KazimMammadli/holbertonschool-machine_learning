0a0f4bb882ca23393a0fa3ec08f39a2a8176eb90
Kazim <kazimammadli@gmail.com>
2025-08-06 12:17:26 +0400

    fixing long line issue

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,7 +5,8 @@
 def matrix_transpose(matrix):
     """Return transpose of a matrix
     second method
-    [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]"""
+    [[matrix[i][j] for i in range(len(matrix))]
+    for j in range(len(matrix[0]))]"""
     new_matrix = []
     for j in range(len(matrix[0])):
         new_row = []
