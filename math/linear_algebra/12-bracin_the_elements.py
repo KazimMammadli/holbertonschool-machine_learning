7c7c4e734b10c56a85afd7d8f252419ec2e3a697
Kazim <kazimammadli@gmail.com>
2025-08-06 21:15:30 +0400

    12.task

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+"""
+This module defines a function for
+elementwise operations.
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Return element-wise addition, subtraction,
+    multiplication, and division.
+    """
+    return (mat1 + mat2, mat1 - mat2,
+            mat1 * mat2, mat1 / mat2)
