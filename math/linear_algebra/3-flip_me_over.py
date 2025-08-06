d75778573f2485776fa2eee52be8ce7d43eea7a3
Kazim <kazimammadli@gmail.com>
2025-08-06 12:15:47 +0400

    3.task

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module defines a function to transpose a matrix."""
+def matrix_transpose(matrix):
+    """Return transpose of a matrix
+    second method
+    [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]"""
+    new_matrix = []
+    for j in range(len(matrix[0])):
+        new_row = []
+        for i in range(len(matrix)):
+            new_row.append(matrix[i][j])
+        new_matrix.append(new_row)          
+    return new_matrix
