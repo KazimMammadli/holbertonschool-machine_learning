3da13a59bfb2eaa0d31f3e43c0ee761bc2534c3e
Kazim <kazimammadli@gmail.com>
2025-08-06 12:56:26 +0400

    6.task

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python3
+"""This module defines a function
+to concatenate two arrays."""
+
+
+def cat_arrays(arr1, arr2):
+    """Return concatenation of two arrays."""
+    return arr1 + arr2
