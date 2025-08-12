df138a1ac7543e30dba3b59ac9b5bd68d63656d5
Kazim <kazimammadli@gmail.com>
2025-08-12 17:34:23 +0000

    docs adding

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,10 +1,17 @@
-#!/usr/env/bin python3
+#!/usr/bin/env python3
+"""This module is used to define integral function."""
+
+
 def poly_integral(poly, C=0):
+    """
+    Return the coefficients representing the
+    integral of the polynomial.
+    """
     if (not isinstance(poly, list) or
        not all(isinstance(coef, (int, float)) for coef in poly) or
        len(poly) == 0 or not isinstance(C, (int, float))):
         return None
-    
-    new_list =[poly[i] / (i + 1)  for i in range(len(poly))]
+
+    new_list = [poly[i] / (i + 1) for i in range(len(poly))]
 
     return [C] + [int(x) if x.is_integer() else x for x in new_list]
