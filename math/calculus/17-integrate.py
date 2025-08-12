25eb9778f31771a225529d23b098874b28a69e46
Kazim <kazimammadli@gmail.com>
2025-08-12 17:41:03 +0000

    handling [0]

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -12,6 +12,9 @@ def poly_integral(poly, C=0):
        len(poly) == 0 or not isinstance(C, (int, float))):
         return None
 
+    if all(coef == 0 for coef in poly):
+        poly = []
+
     new_list = [poly[i] / (i + 1) for i in range(len(poly))]
 
     return [C] + [int(x) if x.is_integer() else x for x in new_list]
