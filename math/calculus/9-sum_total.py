77b2854f6c75d6752c028ec6884502699d18bc20
Kazim <kazimammadli@gmail.com>
2025-08-12 14:14:33 +0000

    9.task

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,9 @@
+#!/usr/env/bin python3
+"""This module defines a function for sum of squares."""
+
+
+def summation_i_squared(n):
+    """Return the sum of square values."""
+    if not isinstance(n, int) or n < 1:
+        return None
+    return int(n * (n + 1) * (2 * n + 1)/6)
