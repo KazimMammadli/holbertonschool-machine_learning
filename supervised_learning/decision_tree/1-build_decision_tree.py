5ca70fd39da3581ada9a13f23aff4dfefc609ee2
KazimMammadli <kazimammadli@gmail.com>
2025-08-22 12:15:23 +0000

    1.task

---
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+""" Number of nodes/leaves in a decision tree."""
+import numpy as np
+
+
+class Node:
+    """A node class containing leaves,roots."""
+
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
+        """Constructor of Node class."""
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Return the maximum depth."""
+        if self.is_leaf:
+            return self.depth
+        left = self.left_child.max_depth_below()\
+            if self.left_child else self.depth
+        right = self.right_child.max_depth_below()\
+            if self.right_child else self.depth
+        return max(left, right)
+
+    def count_nodes_below(self, only_leaves=False):
+        """Count the number of nodes below, only leaves if specified."""
+        if self.is_leaf:
+            return 1
+
+        if only_leaves:
+            if self.left_child:
+                left = self.left_child.count_nodes_below(True)
+            else:
+                left = 0
+            if self.right_child:
+                right = self.right_child.count_nodes_below(True)
+            else:
+                right = 0
+            return left + right
+        else:
+            if self.left_child:
+                left = self.left_child.count_nodes_below(False)
+            else:
+                left = 0
+            if self.right_child:
+                right = self.right_child.count_nodes_below(False)
+            else:
+                right = 0
+            return 1 + left + right
+
+
+class Leaf(Node):
+    """Leaf class."""
+
+    def __init__(self, value, depth=None):
+        """Constructor of Leaf class."""
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Return the depth of the leaf."""
+        return self.depth
+
+    def count_nodes_below(self, only_leaves=False):
+        """Return the count of a leaf."""
+        return 1
+
+
+class Decision_Tree():
+    """Decision Tree class."""
+
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
+        """Constructor of decision tree class."""
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """Return the maximum depth of tree."""
+        return self.root.max_depth_below()
+
+    def count_nodes(self, only_leaves=False):
+        """Return the count of leaves."""
+        return self.root.count_nodes_below(only_leaves=only_leaves)
