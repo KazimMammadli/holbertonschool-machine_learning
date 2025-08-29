26fbdeec3a7c26dbb9e052e15d22b5790dabc986
MammadliKazim <kazim.memmedli.005@gmail.com>
2025-08-29 14:49:47 +0000

    0.task

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Neuron"""
+
+
+class Neuron:
+    """Neuron class"""
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
