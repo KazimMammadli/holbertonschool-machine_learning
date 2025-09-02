90c2a3446f20e3bc9b3b85badffbbd799c745aff
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-02 12:27:54 +0000

    checking Y

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """Convert a one-hot matrix into a vector of labels."""
-    if not isinstance(Y, np.ndarray) or one_hot.ndim != 2:
+    if not isinstance(one_hot, np.ndarray) or one_hot.ndim != 2:
         return None
     try: 
         return np.argmax(one_hot, axis = 0)
