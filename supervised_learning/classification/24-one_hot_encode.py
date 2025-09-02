3ef621a316cb799e88f186eade02567637cc47cd
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-02 12:25:01 +0000

    checking ndarray or not

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """Convert a numeric label vector into a one-hot matrix."""
+    if not isinstance(Y, np.ndarray):
+        return None
     try:
         Y_One_Hot = np.eye(classes)[Y]
         return Y_One_Hot.T
