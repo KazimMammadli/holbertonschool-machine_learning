eae41946461f64cdfd0cd5cff20b1d6e6194749c
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-05 22:06:02 +0000

    6.task

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Early stoppping and patience parameters."""
+"""Early Stopping"""
 import tensorflow.keras as K
 
 
@@ -7,8 +7,12 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, verbose=True, shuffle=False):
     """Return the History object generated after training the model."""
+    callbacks = []
+    if early_stopping and validation_data is not None:
+        callbacks.append(K.callbacks.EarlyStopping(monitor="val_loss",
+                                                   patience=patience))
     history = network.fit(data, labels, batch_size=batch_size,
                           epochs=epochs, validation_data=validation_data,
-                          early_stopping=early_stopping, patience=patience,
-                          shuffle=shuffle, verbose=verbose)
+                          callbacks=callbacks, shuffle=shuffle,
+                          verbose=verbose)
     return history
