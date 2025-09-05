6dd3e8ea396cf82b67a4e6bf1db153fae14314ec
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-05 20:05:03 +0000

    5.task

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Validation data"""
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """Return the History object generated after training the model."""
+    history = network.fit(data, labels, batch_size=batch_size,
+                          epochs=epochs, validation_data=validation_data,
+                          shuffle=shuffle, verbose=verbose)
+    return history
