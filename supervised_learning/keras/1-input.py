c6432c7d5f6cbb833729a3a0070cba16e18b763d
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-05 11:41:41 +0000

    fixing imports

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
-from tensorflow.keras.layers import Dense, Input, Dropout
-from tensorflow.keras.models import Model
-from tensorflow.keras import regularizers
+import tensorflow.keras as K
 """Input class."""
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Building model with Input class."""
-    regularizer = regularizers.L2(lambtha)
+    regularizer = K.regularizers.L2(lambtha)
     dropout = 1 - keep_prob
-    iinputs = Input(shape=(nx,))
+    inputs = K.layers.Input(shape=(nx,))
     x = inputs
 
     for i in range(len(layers)):
-        x = Dense(units=layers[i],
-                  activation=activations[i],
-                  kernel_regularizer=regularizer)(x)
+        x = K.layers.Dense(units=layers[i],
+                           activation=activations[i],
+                           kernel_regularizer=regularizer)(x)
 
         if i < len(layers) - 1:
-            x = Dropout(rate=dropout)(x)
+            x = K.layers.Dropout(rate=dropout)(x)
 
-    model = Model(inputs=inputs, outputs=x)
+    model = K.models.Model(inputs=inputs, outputs=x)
     return model
