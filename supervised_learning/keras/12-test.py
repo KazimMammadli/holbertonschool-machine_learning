5b3040be762c051870ba160f28ce0ae781af63d2
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-06 08:30:29 +0000

    removing whitespace

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -6,5 +6,5 @@ import tensorflow.keras as K
 def test_model(network, data, labels, verbose=True):
     """Test model."""
     verbose = 1 if verbose else 0
-    result = network.evaluate(data, labels, verbose=verbose )
+    result = network.evaluate(data, labels, verbose=verbose)
     return result
