46c71f8cc4df858322ac5dedfce400848c582ffb
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-11 20:47:28 +0000

    adding shuffle func

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -2,7 +2,7 @@
 """Mini-Batch"""
 import numpy as np
 
-
+shuffle_data = __import__('2-shuffle_data').shuffle_data
 def create_mini_batches(X, Y, batch_size):
     """Create mini batches."""
     X, Y = shuffle_data(X, Y)
