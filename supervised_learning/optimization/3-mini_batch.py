d265b09c42dbc2bb37bc4fe08c132fb487644a7c
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-11 20:51:35 +0000

    pycode

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -2,7 +2,10 @@
 """Mini-Batch"""
 import numpy as np
 
+
 shuffle_data = __import__('2-shuffle_data').shuffle_data
+
+
 def create_mini_batches(X, Y, batch_size):
     """Create mini batches."""
     m = X.shape[0]
