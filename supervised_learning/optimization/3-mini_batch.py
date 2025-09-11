afccd5e88e84bb9643993cd676c62ca08bb29b05
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-11 20:49:55 +0000

    adding num of samples

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -5,6 +5,7 @@ import numpy as np
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 def create_mini_batches(X, Y, batch_size):
     """Create mini batches."""
+    m = X.shape[0]
     X, Y = shuffle_data(X, Y)
     mini_batches = []
     for i in range(0, m, batch_size):
