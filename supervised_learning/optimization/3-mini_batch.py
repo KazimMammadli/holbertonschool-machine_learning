8673331f567ad10be186e40a5e985fdf9308f3eb
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-11 20:44:38 +0000

    3.task

---
+++ supervised_learning/optimization/3-mini_batch.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Mini-Batch"""
+import numpy as np
+
+
+def create_mini_batches(X, Y, batch_size):
+    """Create mini batches."""
+    X, Y = shuffle_data(X, Y)
+    mini_batches = []
+    for i in range(0, m, batch_size):
+        X_batch = X[i:i + batch_size]
+        Y_batch = Y[i:i + batch_size]
+        mini_batches.append((X_batch, Y_batch))
+
+    return mini_batches
