3aff6e3bcb3ca231617521165efd93ca59a355a2
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-12 00:17:51 +0000

    rho=beta2 not momentum

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -6,6 +6,6 @@ import tensorflow as tf
 def create_RMSProp_op(alpha, beta2, epsilon):
     """Return optimizer."""
     optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha,
-                                            momentum=beta2,
+                                            rho=beta2,
                                             epsilon=epsilon)
     return optimizer
