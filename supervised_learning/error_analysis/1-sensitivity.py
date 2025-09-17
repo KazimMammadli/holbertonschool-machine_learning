42dd6a50e5ab7a3fd24597b72884bc2e485312a5
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-17 21:10:39 +0000

    1.task

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -7,7 +7,7 @@ def sensitivity(confusion):
     """Return a numpy.darray of shape (classes,) containing
     the sensitivity of each class."""
     n = confusion.shape[0]
-    sum_cols = np.sum(confusion, axis=0)
+    sum_cols = np.sum(confusion, axis=1)
     return np.array([
         (confusion[i][i] / sum_cols[i]) if sum_cols[i] != 0 else 0
         for i in range(n)
