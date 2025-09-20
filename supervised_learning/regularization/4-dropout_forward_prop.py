0977787ed34441661c37ae8bf8ea1d006d72989b
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-20 23:52:36 +0000

    4.task

---
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Forward Propagation with Dropout."""
+import tensorflow as tf
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    cache["A0"] = X
+    for i in range(1, L):
+        W = weights["W{}".format(i)]
+        A = cache["A{}".format(i-1)]
+        b = weights["b{}".format(i)]
+        z = np.dot(W, A) + b
+        A = np.tanh(z)   # we compute A to match the shape with D
+        D = (np.random.rand(*A.shape) < keep_prob).astype(int)
+        A = A * D / keep_prob
+        cache["A{}".format(i)] = A
+        cache["D{}".format(i)] = D
+
+    # last layer
+    z = (np.dot(weights["W{}".format(L)], cache["A{}".format(L-1)]) +
+         weights["b{}".format(L)])
+
+    exp_z = np.exp(z - np.max(z, axis=0, keepdims=True))
+    A = exp_z / np.sum(exp_z, axis=0, keepdims=True)
+    cache["A{}".format(L)] = A
+    return cache
