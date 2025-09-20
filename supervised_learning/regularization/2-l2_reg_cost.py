4bd613835014fed9c5f78efb8ff00da6337ef044
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-20 22:49:24 +0000

    2.task

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""L2 Regularization Cost."""
+import numpy as np
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, model):
+    """Return a tensor containing the cost for each layer
+    including its L2 regularization."""
+    l2_losses = model.losses
+
+    # a list of new tensors, each representing a "total cost" per layer.
+    total_costs_per_layer = [cost + l for l in l2_losses]
+
+    # Finally, we stack this list of tensors into a single tensor to produce
+    # the desired output
+    return tf.stack(total_costs_per_layer)
