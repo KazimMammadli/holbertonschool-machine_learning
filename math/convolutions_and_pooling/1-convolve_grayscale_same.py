bb073aa1976e9a0476bac80a4844e5e6a4a39195
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-27 00:15:29 +0000

    make it work for all kernel sizes even or odd

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -7,17 +7,20 @@ def convolve_grayscale_same(images, kernel):
     """Return a numpy.ndarray containing the convolved images"""
     m, h, w = images.shape
     kh, kw = kernel.shape
-    padding_h = (kh - 1) // 2
-    padding_w = (kw - 1) // 2
-    new_w = w   # remains the same
-    new_h = h   # remains the same
-    convolved = np.zeros((m, new_h, new_w))
-    padded_images = np.pad(images,
-                           pad_width=((0, 0), (padding_h, padding_h),
-                                      (padding_w, padding_w)),
-                           mode='constant', constant_values=0)
-    for i in range(new_h):
-        for j in range(new_w):
+    # for both even and odd kernel sizes
+    ph_top = kh // 2
+    ph_bottom = kh - 1 - ph_top
+    pw_left = kw // 2
+    pw_right = kw - 1 - pw_left
+    convolved = np.zeros((m, h, w))
+    padded_images = np.pad(
+        images,
+        pad_width=((0, 0), (ph_top, ph_bottom), (pw_left, pw_right)),
+        mode='constant',
+        constant_values=0
+    )
+    for i in range(h):
+        for j in range(w):
             convolved[:, i, j] = np.sum(padded_images[:, i:i + kh, j:j + kw] *
                                         kernel, axis=(1, 2))
     return convolved
