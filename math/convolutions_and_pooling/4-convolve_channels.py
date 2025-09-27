26606e014d9360da63c8d5393065449b041db1cb
KazimMammadli <kazim.memmedli.005@gmail.com>
2025-09-27 00:23:23 +0000

    changing formula for ph and pw

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -7,9 +7,10 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """Return a numpy.ndarray containing the convolved images"""
     m, h, w, c = images.shape
     kh, kw, c = kernel.shape
+    sh, sw = stride
     if padding == 'same':
-        ph = (kh - 1) // 2
-        pw = (kw - 1) // 2
+        ph = int(np.ceil(((h - 1) * sh + kh - h) / 2))
+        pw = int(np.ceil(((w - 1) * sw + kw - w) / 2))
 
     if padding == 'valid':
         ph = 0
@@ -18,7 +19,6 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     if isinstance(padding, tuple):
         ph, pw = padding
 
-    sh, sw = stride
     new_w = (w + 2 * pw - kw) // sw + 1
     new_h = (h + 2 * ph - kh) // sh + 1
     convolved = np.zeros((m, new_h, new_w))
