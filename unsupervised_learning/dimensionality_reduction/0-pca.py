9ea343c54dac606718658480b2a235748900d82a
Kazim Mammadli <kazim.memmedli.005@gmail.com>
2025-11-30 15:30:04 +0400

    0.task
    Refine PCA function documentation and improve comments.

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-"""PCA"""
+"""useless comments"""
 import numpy as np
 
 
 def pca(X, var=0.95):
     """
-    Compute the PCA.
+    Compute the PCA, to get var% of the var explain
     """
     U, S, Vt = np.linalg.svd(X)
+
     total_var_explain = 0
     idx = 0
+
     normal_S = S / np.sum(S)
 
     for var_explain in normal_S:
