1dfc8c735de2f8c6aec9d8bb10e1516f45b3d399
Kazim Mammadli <kazim.memmedli.005@gmail.com>
2025-11-30 15:43:02 +0400

    7.taskv2

--- unsupervised_learning/dimensionality_reduction/7-cost.py
+++ unsupervised_learning/dimensionality_reduction/7-cost.py
@@ -4,8 +4,6 @@ import numpy as np
 
 
 def cost(P, Q):
-    """
-    Calculates the cost of the t-SNE transformation
-    """
-    return (np.sum(np.maximum(P, 1e-12) * np.log(np.maximum(P, 1e-12)
-                                                 / (np.maximum(Q, 1e-12)))))
+    """Calculates the cost of the t-SNE transformation."""
+    return (np.sum(np.maximum(P, 1e-12) *
+                   np.log(np.maximum(P, 1e-12) / (np.maximum(Q, 1e-12)))))
