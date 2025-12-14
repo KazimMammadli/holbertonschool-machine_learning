bdbfbc9975e5a7c75eef4c9fcf43b43ecc6084ab
Kazim Mammadli <kazim.memmedli.005@gmail.com>
2025-12-14 20:57:46 +0400

    2.task

---
+++ unsupervised_learning/hyperparameter_tuning/2-gp.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Update Gaussian Process"""
+import numpy as np
+
+
+class GaussianProcess:
+    """
+    Gaussian Process Class.
+    """
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Class Constructor.
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """
+        Calculate the covariance kernel matrix between two matrices.
+        """
+        dist = np.sum(X1 ** 2, axis=1).reshape(-1, 1) + \
+            np.sum(X2 ** 2, axis=1) - 2 * np.dot(X1, X2.T)
+        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * dist)
+
+    def predict(self, X_s):
+        """
+        Predict the mean and standard deviation of
+        points in a Gaussian process.
+        """
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+
+        K_inv = np.linalg.inv(self.K)
+
+        mu_s = np.matmul(np.matmul(K_s.T, K_inv), self.Y)
+        covariance_s = K_ss - np.matmul(np.matmul(K_s.T, K_inv), K_s)
+
+        return mu_s.reshape(1, -1), np.diagonal(covariance_s)
+
+    def update(self, X_new, Y_new):
+        """
+        Update the value with the news predicted.
+        """
+        self.X = np.vstack((self.X, X_new))
+        self.Y = np.vstack((self.Y, Y_new))
+        self.K = self.kernel(self.X, self.X)
