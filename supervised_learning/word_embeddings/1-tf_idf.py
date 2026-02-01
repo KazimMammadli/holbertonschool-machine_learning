bf6bae3c178a02ffbb6f3a50c8cb131a21e960ac
Kazim Mammadli <kazim.memmedli.005@gmail.com>
2026-02-01 23:24:40 +0400

    1.task

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -5,17 +5,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
-    '''creates a bag of words embedding matrix:
-    Args:
-        sentences is a list of sentences to analyze
-        vocab is a list of the vocabulary words to use for the analysis
-            If None, all words within sentences should be used
-    Returns: embeddings, features
-        embeddings is a numpy.ndarray of shape (s, f) containing the embeddings
-            s is the number of sentences in sentences
-            f is the number of features analyzed
-        features is a list of the features used for embeddings
-    '''
-    vectorizer = TfidfVectorizer(vocabulary=vocab)
-    X = vectorizer.fit_transform(sentences)
-    return (X.toarray(), vectorizer.get_feature_names())
+    """Creates a TFIDF embedding matrix"""
+    tfids_vector = TfidfVectorizer(vocabulary=vocab)
+    embed = tfids_vector.fit_transform(sentences)
+    features = tfids_vector.get_feature_names_out()
+    return embed.toarray(), features
