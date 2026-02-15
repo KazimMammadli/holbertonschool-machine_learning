31de9d4c9e4a38b82abe3b651ed2c24fd35854fe
Kazim Mammadli <kazim.memmedli.005@gmail.com>
2026-02-15 13:47:02 +0400

    0.taskv2

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,75 +1,71 @@
 #!/usr/bin/env python3
 """
-Dataset module for machine translation (Portuguese to English).
+Dataset module for Portuguese to English machine translation.
 
-This module defines the Dataset class which loads the TED Talks
-Portuguese-English dataset and prepares pretrained tokenizers
-for both languages.
+Defines the Dataset class which loads the TED HRLR translation
+dataset and initializes pretrained tokenizers.
 """
 
 import tensorflow_datasets as tfds
-from transformers import AutoTokenizer
+import transformers
 
 
 class Dataset:
     """
-    Dataset class for loading and preparing translation data.
+    Loads and prepares the translation dataset.
 
     Attributes:
-        data_train (tf.data.Dataset): Training split of dataset.
-        data_valid (tf.data.Dataset): Validation split of dataset.
-        tokenizer_pt (transformers tokenizer): Portuguese tokenizer.
-        tokenizer_en (transformers tokenizer): English tokenizer.
+        data_train: Training dataset split.
+        data_valid: Validation dataset split.
+        tokenizer_pt: Portuguese tokenizer.
+        tokenizer_en: English tokenizer.
     """
 
     def __init__(self):
         """
         Initializes the Dataset instance.
 
-        Loads the TED HRLR Portuguese-to-English dataset and
-        initializes pretrained tokenizers trained on the training set.
+        Loads the TED HRLR Portuguese-English dataset and creates
+        pretrained tokenizers.
         """
         self.data_train = tfds.load(
-            'ted_hrlr_translate/pt_to_en',
-            split='train',
+            "ted_hrlr_translate/pt_to_en",
+            split="train",
             as_supervised=True
         )
 
         self.data_valid = tfds.load(
-            'ted_hrlr_translate/pt_to_en',
-            split='validation',
+            "ted_hrlr_translate/pt_to_en",
+            split="validation",
             as_supervised=True
         )
 
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train
-        )
+        self.tokenizer_pt, self.tokenizer_en = \
+            self.tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
         """
-        Creates pretrained sub-word tokenizers for the dataset.
+        Creates pretrained sub-word tokenizers.
 
-        Uses pretrained BERT tokenizers:
-        - Portuguese: neuralmind/bert-base-portuguese-cased
-        - English: bert-base-uncased
-
-        The vocabulary size is limited to 2**13.
+        Uses:
+        - neuralmind/bert-base-portuguese-cased
+        - bert-base-uncased
 
         Args:
-            data (tf.data.Dataset): Dataset formatted as (pt, en).
+            data: tf.data.Dataset formatted as (pt, en).
 
         Returns:
             tokenizer_pt: Portuguese tokenizer.
             tokenizer_en: English tokenizer.
         """
-        tokenizer_pt = AutoTokenizer.from_pretrained(
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
             "neuralmind/bert-base-portuguese-cased",
-            model_max_length=2**13
+            model_max_length=2 ** 13
         )
 
-        tokenizer_en = AutoTokenizer.from_pretrained(
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
             "bert-base-uncased",
-            model_max_length=2**13
+            model_max_length=2 ** 13
         )
 
         return tokenizer_pt, tokenizer_en
